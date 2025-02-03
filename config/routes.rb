4c8cd3a65d0cb31a5003809d280593f09a023e21
5may-sytk <189351572+5may-sytk@users.noreply.github.com>
2025-02-03 17:51:22 +0000

    devise導入、admin/public作成、ゲストユーザー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions:      'admin/sessions',
+    passwords:     'admin/passwords',
+  }
+
+  devise_for :users, skip: [:passwords], controllers: {
+    sessions:      'public/sessions',
+    passwords:     'public/passwords',
+    registrations: 'public/registrations'
+  }
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
